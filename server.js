090eaf84774960ef81eb9f9975c7d6731c6f5511
sumanth-create1 <sajjasatyasumanth@gmail.com>
2026-07-20 12:09:40 +0530

    created userSchema and user model

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ import 'dotenv/config';
 import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
 
+connectDB();
 
 const app = express();
 const port = process.env.PORT || 5000
-connectDB();
+
 
 app.use(express.json());
 app.use(cookieParser());
