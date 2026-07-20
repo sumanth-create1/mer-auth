8dd26f68693b0faea152b860db026e1138688bd2
sumanth-create1 <sajjasatyasumanth@gmail.com>
2026-07-20 10:53:43 +0530

    connected database to the server

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ import express from "express";
 import cors from "cors";
 import 'dotenv/config';
 import cookieParser from "cookie-parser";
+import connectDB from "./config/db.js";
+
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000
+connectDB();
 
 app.use(express.json());
 app.use(cookieParser());
