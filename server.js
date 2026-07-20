6c163c9c3147c1e591f05f09a887b6c7e6cbd638
sumanth-create1 <sajjasatyasumanth@gmail.com>
2026-07-20 10:30:09 +0530

    setting up the server

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import 'dotenv/config';
+import cookieParser from "cookie-parser";
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({credentials:true}));
+
+app.use("/" , (req,res) => {
+    res.send("API is working!!");
+})
+
+app.listen(port , () => {
+    console.log(`server has started on ${port}`);
+});
